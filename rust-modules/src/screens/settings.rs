gnacho/butaca

feat(i18n): sweep the whole visible chrome - hero, detail, hud, search, login, settings, library

rust-modules/src/ui/settings.rs
@@ -90,8 +90,8 @@ fn rebuild(sel: i32) {
         let n = crate::browse::pinned_count();
         sections.push(
             Section::new(crate::i18n::t("Home")).row(
-                Row::new("Home screen")
-                    .detail("Choose which libraries contribute shelves.")
+                Row::new(crate::i18n::t("Home screen"))
+                    .detail(crate::i18n::t("Choose which libraries contribute shelves."))
                     .value(format!(
                         "{n} {}",
                         if n == 1 { "library" } else { "libraries" }
@@ -104,21 +104,21 @@ fn rebuild(sel: i32) {
     sections.push(
         Section::new("Privacy")
             .row(
-                Row::new("Privacy & data")
-                    .detail("Optional reports, privacy information and local data.")
+                Row::new(crate::i18n::t("Privacy & data"))
+                    .detail(crate::i18n::t("Optional reports, privacy information and local data."))
                     .chevron(true),
             )
             .row(
-                Row::new("Legal notices")
-                    .detail("Privacy, licences, source code, trademarks and contact.")
+                Row::new(crate::i18n::t("Legal notices"))
+                    .detail(crate::i18n::t("Privacy, licences, source code, trademarks and contact."))
                     .chevron(true),
             ),
     );
     actions.extend([Action::Privacy, Action::Legal]);
     sections.push(
-        Section::new("System").row(
-            Row::new(if cfg!(feature = "jellyfin") { "About butaca" } else { "About PlxNative" })
-                .detail("Version, copyright and project information.")
+        Section::new(crate::i18n::t("System")).row(
+            Row::new(if cfg!(feature = "jellyfin") { crate::i18n::t("About butaca") } else { "About PlxNative" })
+                .detail(crate::i18n::t("Version, copyright and project information."))
                 .chevron(true),
         ),
     );

GLinnik21/plx-native

rust-modules/src/screens/settings.rs
@@ -1042,18 +1042,18 @@ impl RootPage {
         sections.push(
             Section::new("Privacy")
                 .row(
-                    Row::new("Privacy & data")
-                        .detail("Optional reports, privacy information and local data.")
+                    Row::new(crate::i18n::t("Privacy & data"))
+                        .detail(crate::i18n::t("Optional reports, privacy information and local data."))
                         .chevron(true),
                 )
                 .row(
-                    Row::new("Legal notices")
-                        .detail("Privacy, licences, source code, trademarks and contact.")
+                    Row::new(crate::i18n::t("Legal notices"))
+                        .detail(crate::i18n::t("Privacy, licences, source code, trademarks and contact."))
                         .chevron(true),
                 ),
         );
         actions.extend([Action::Privacy, Action::Legal]);
-        let mut system = Section::new("System");
+        let mut system = Section::new(crate::i18n::t("System"));
         // A one-person account already skips the picker; the switch only changes a multi-user boot.
         if signed_in && multi_user {
             system = system.row(
@@ -1072,8 +1072,8 @@ impl RootPage {
             actions.push(Action::TrailerAutoplay);
         }
         system = system.row(
-            Row::new("About PlxNative")
-                .detail("Version, copyright and project information.")
+            Row::new(crate::i18n::t("About PlxNative"))
+                .detail(crate::i18n::t("Version, copyright and project information."))
                 .chevron(true),
         );
         sections.push(system);